59a3ba0f4430f5e30a7599a576afd4947122f702
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-08-02 14:21:51 -0400

    add axios and thunk, load mock data from mockapi.io

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
 import App from './components/App';
+import {loadItems} from './actions/itemActions';
 
 const store = configureStore();
+store.dispatch(loadItems());
 
 render((
     <Provider store={store}>

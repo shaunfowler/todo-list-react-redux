2201715890c6d4b6bdb3629025e712d758a8c8f5
Shaun Fowler <Shaun.Fowler@qlik.com>
2017-08-03 16:46:16 -0400

    async write to items API

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import * as types from './actionTypes';
 
-export function createItem(item) {
-    return {type: types.CREATE_ITEM, item};
+export function createItemSuccess(item) {
+    return {type: types.CREATE_ITEM_SUCCESS, item};
 }
 
 export function loadItemsSuccess(items) {
@@ -10,8 +10,16 @@ export function loadItemsSuccess(items) {
 }
 
 export function loadItems() {
-    return dispatch => axios.get('http://5981fe84139db000114a2df3.mockapi.io/items').then((items) => {
-        dispatch(loadItemsSuccess(items.data));
+    return dispatch => axios.get('http://5981fe84139db000114a2df3.mockapi.io/items').then((response) => {
+        dispatch(loadItemsSuccess(response.data));
+    }).catch((error) => {
+        throw error;
+    });
+}
+
+export function createItem(item) {
+    return dispatch => axios.post('http://5981fe84139db000114a2df3.mockapi.io/items', item).then((response) => {
+        dispatch(createItemSuccess(response.data));
     }).catch((error) => {
         throw error;
     });
